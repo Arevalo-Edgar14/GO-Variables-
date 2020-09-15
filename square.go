431cba46a880fe647095093988d78b8c28cb44f6
Arevalo-Edgar14 <edgar.arevalo@alumnos.udg.mx>
2020-09-14 23:23:02 -0500

    read a variable and print the square area

---
+++ square.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Ingresa cuanto mide uno de los lados del cuadrado")
+	var side int
+	fmt.Scanf("%d", &side)
+	ftm.Println("El area del cuadrado es: %d", side*side)
+}
