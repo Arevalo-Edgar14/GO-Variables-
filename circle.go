816e6991692657bc13e8f05dbd7e29595a54f024
Arevalo-Edgar14 <edgar.arevalo@alumnos.udg.mx>
2020-09-15 00:04:05 -0500

    for math library are need to use float64 variables

---
+++ circle.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var radio float64
+	fmt.Println("Ingresa cuanto mide el radio")
+	fmt.Scanf("%f", &radio)
+	area := math.Pi * radio * radio
+	fmt.Println("El area del triangulo es: ", area)
+}
